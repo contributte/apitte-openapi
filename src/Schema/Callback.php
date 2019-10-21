561f48aa767b32d6c214af0eb9b5a2d1e6f84897
Marek Bartoš <bartos.developer152@gmail.com>
2019-10-21 22:12:49 +0200

    Callback: initial implementation

--- src/Schema/Callback.php
+++ src/Schema/Callback.php
@@ -5,12 +5,31 @@ namespace Apitte\OpenApi\Schema;
 class Callback
 {
 
+	/** @var mixed[] */
+	private $data = [];
+
+	/**
+	 * @param mixed[] $data
+	 */
+	public function __construct(array $data)
+	{
+		$this->data = $data;
+	}
+
+	/**
+	 * @param mixed[] $data
+	 */
+	public static function fromArray(array $data): Callback
+	{
+		return new Callback($data);
+	}
+
 	/**
 	 * @return mixed[]
 	 */
 	public function toArray(): array
 	{
-		return [];
+		return $this->data;
 	}
 
 }
